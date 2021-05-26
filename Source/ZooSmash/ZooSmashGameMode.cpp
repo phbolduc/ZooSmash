0216d29be1440b5b38ee68ce2174514409065e8f
Philippe <philbolduc200@gmail.com>
2021-05-26 11:15:31 -0400

    added elephant character

--- Source/ZooSmash/ZooSmashGameMode.cpp
+++ Source/ZooSmash/ZooSmashGameMode.cpp
@@ -7,7 +7,7 @@
 AZooSmashGameMode::AZooSmashGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ElephantCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
